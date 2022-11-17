02ca83d1161e489da63bea82cff5bf91251357f3
Philippe Virouleau <philippe.virouleau@inria.fr>
2022-11-17 10:16:28 +0100

    Introduce the getRange helper

--- src/aSSA/Utils.h
+++ src/aSSA/Utils.h
@@ -1,7 +1,7 @@
 #ifndef UTILS_H
 #define UTILS_H
 
-#include "PTACallGraph.h"
+#include "llvm/ADT/STLExtras.h"
 #include "llvm/Analysis/PostDominators.h"
 
 #include <vector>
@@ -41,5 +41,25 @@ unsigned getBBSetIntersectionSize(const std::set<const llvm::BasicBlock *> S1,
 unsigned
 getInstSetIntersectionSize(const std::set<const llvm::Instruction *> S1,
                            const std::set<const llvm::Instruction *> S2);
+
+// This is a small helper to always get a valid - possibly empty - range
+// contained in a const container.
+// Motivation: operator[] is not const, llvm's lookup is const but obviously
+// returns a value and not a reference.
+// This attempts to bring the best of both approach: provide a range over the
+// contained set/map, using a default empty set/map if it does not exist in
+// the map.
+template <typename Container, typename KeyTy>
+auto getRange(Container const &C, KeyTy K) {
+  static_assert(std::is_pointer_v<KeyTy>,
+                "getRange must have a KeyTy which is a pointer");
+  static decltype(C.lookup(K)) const Empty;
+  auto It = C.find(K);
+  if (It != C.end()) {
+    return llvm::make_range(It->second.begin(), It->second.end());
+  } else {
+    return llvm::make_range(Empty.begin(), Empty.end());
+  }
+}
 
 #endif /* UTILS_H */

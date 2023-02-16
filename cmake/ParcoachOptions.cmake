f2e093fb2dcfab003b7400bc8f6d0c3ace75f106
Philippe Virouleau <philippe.virouleau@inria.fr>
2023-02-16 11:16:43 +0100

    Simplify CMake options

--- cmake/ParcoachOptions.cmake
+++ cmake/ParcoachOptions.cmake
@@ -3,14 +3,7 @@ option(PARCOACH_ENABLE_COVERAGE "Enable code coverage.")
 option(PARCOACH_ENABLE_TIDY "Enable clang-tidy when building parcoach.")
 option(PARCOACH_BUILD_SHARED "Build parcoach as a shared library." ON)
 option(PARCOACH_ENABLE_TESTS "Enable PARCOACH test targets." ON)
-option(PARCOACH_LINK_DYLIB "Enable linking PARCOACH to the LLVM dylib." ON)
 
 if(PARCOACH_ENABLE_COVERAGE AND NOT PARCOACH_ENABLE_TESTS)
   message(FATAL_ERROR "You must enable tests to enable coverage.")
-endif()
-
-if(NOT PARCOACH_LINK_DYLIB AND PARCOACH_BUILD_SHARED)
-  message(FATAL_ERROR "PARCOACH is set to not link against the LLVM dylib, "
-    "building shared libraries must be turned off by passing "
-    "-DPARCOACH_BUILD_SHARED=OFF.")
 endif()

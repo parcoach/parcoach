ebd9f38d79bec17822741fa594cd8fe27523fae7
Philippe Virouleau <philippe.virouleau@inria.fr>
2023-05-09 19:21:58 +0200

    Disable MBI outside of coverage.

--- caches/Coverage.cmake
+++ caches/Coverage.cmake
@@ -2,6 +2,10 @@ set(PARCOACH_ENABLE_COVERAGE ON CACHE BOOL "")
 set(PARCOACH_ENABLE_INSTRUMENTATION ON CACHE BOOL "")
 set(PARCOACH_ENABLE_FORTRAN ON CACHE BOOL "")
 set(PARCOACH_BUILD_SHARED OFF CACHE BOOL "")
+
+# For coverage we definitely want that.
+set(PARCOACH_ENABLE_MBI_TESTS ON CACHE BOOL "")
+
 set(CMAKE_EXPORT_COMPILE_COMMANDS ON CACHE BOOL "")
 # We want -g and as few optimizations as possible to be able to have the most
 # accurate line coverage.
